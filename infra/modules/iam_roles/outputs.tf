c4e58c400ab3e289dd6ce8e4ed54eb2aad4c2efe
DiegoOrellanoDataGit <diegoorellanodesarrollo@outlook.com>
2026-09-08 18:41:14 -0300

    Delete infra/modules/iam_roles directory

--- infra/modules/iam_roles/outputs.tf
+++
@@ -1,9 +0,0 @@
-output "flink_role_name" {
-  description = "Nombre del rol IAM para flink"
-  value = aws_iam_role.flink_role.name
-}
-
-output "flink_role_arn" {
-  description = "ARN del rol IAM para flink"
-  value = aws_iam_role.flink_role.arn
-}
